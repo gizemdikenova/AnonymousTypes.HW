9aaeca7919d67391777a0f5ce19d9fd3fa755e30
Gizem Dikenova <dikenovagizem@gmail.com>
2021-01-24 18:19:03 +0300

    Add project files.

---
+++ AnonymousTypes.HW/student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousTypes.HW
+{
+    class student
+    {
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Id { get; set; }
+
+
+    }
+}
